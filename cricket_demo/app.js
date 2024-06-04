e728deb0c5956c6a1af87b067be755212a348e69
Sean Moore <smoore@mozeus.com>
2024-06-04 15:52:18 -0400

    worked on hamburger menu

--- cricket_demo/app.js
+++ cricket_demo/app.js
@@ -1,11 +1,10 @@
 document.querySelector("#Submit").addEventListener("click", handleClick);
 
 function handleClick() {
-    var rules = document.getElementById("Agree")
-    if (rules.checked == false) {
-        document.querySelector(".OFR label").style.color = "red";
-    }
-    else {
-        document.querySelector(".OFR label").style.color = "black";
-    }
+  var rules = document.getElementById("Agree");
+  if (rules.checked == false) {
+    document.querySelector(".OFR label").style.color = "red";
+  } else {
+    document.querySelector(".OFR label").style.color = "black";
+  }
 }
